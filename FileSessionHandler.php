934959e3156a4f21a9eaa2ca64ed3b1e320d9a9b
a-le <alexandre.lemiere@gmail.com>
2015-09-04 09:57:09 +0200

    Update FileSessionHandler.php

--- FileSessionHandler.php
+++ FileSessionHandler.php
@@ -2,25 +2,48 @@
 /**
  * User: a-le
  * Date: 2013-11-16
- * File based session storage, with no LOCK_EX on file (opposite of PHP internal session handler).
- * The objective is to improve speed for cases of concurrent session read access, as PHP default session handler is sequential.
- * read session data from file (1 file by user session), and write it when modified
- * note that as we don't set LOCK_EX on file between reading and writing :
- *    in case of concurrent access & modification to the same user session data, overwrite will happen
- *   ( AFAIK database based sessionHandlers like memcache, memcached  get that same behaviour )
+ * version : 1.0
+ * PHP >= 5.4
+ * File based session storage, with no session locking.
+ * PHP internal session handler do session locking.
+ *
+ * This script is based on php example on how to implement SessionHandlerInterface see http://php.net/manual/en/class.sessionhandlerinterface.php
+ * with a notable difference :
+ * - we do not write data back to file when data did not change
+ *
+ * Objectives :
+ * ------------
+ * When loading images (and other static content) the browser may decide to use multiple TCP connections (concurrent) to get the data as fast as possible.
+ * This also happens when using AJAX.
+ * This may (and will most likely) lead to different workers (threads) on the web server answering these concurrent requests concurrently.
+ *
+ * The objective of this script is to improve speed for those cases of concurrent requests.
+ * The drawback is race conditions may happen if you need to write data to session.
+ *
+ *
+ * References :
+ * ------------
+ * - http://php.net/manual/en/function.session-set-save-handler.php
+ * - about session locking : https://www.leaseweblabs.com/2013/10/symfony2-memcache-session-locking/
+ * - about race conditions : http://thwartedefforts.org/2006/11/11/race-conditions-with-ajax-and-php-sessions/
+ *
  *
  * How to use :
  * -------------
  * session_set_save_handler( new FileSessionHandler(), true );
  * session_start();
+ *
  * // use session as usual...
+ *
  */
-class FileSessionHandler implements SessionHandlerInterface {
+class FileSessionHandler implements \SessionHandlerInterface {
     private $savePath, $data;
 
-    // this does not create $savePath if not exists
     public function open($savePath, $sessionName) {
         $this->savePath = $savePath;
+        if ( !is_dir($this->savePath) ) {
+            mkdir($this->savePath, 0777);
+        }
         return true;
     }
 
@@ -42,7 +65,7 @@ class FileSessionHandler implements SessionHandlerInterface {
 
         // check if data has changed since first read
         if ( $data !== $this->data ) {
-            // write data (even if actual file session data is not the same as when we first read it)
+            // write data
             return @file_put_contents($filename, $data, LOCK_EX) === false ? false : true;
         }
         else return @touch($filename);// let's not forget to postpone session garbage collection
@@ -66,3 +89,4 @@ class FileSessionHandler implements SessionHandlerInterface {
         return true;
     }
 }
+?>
